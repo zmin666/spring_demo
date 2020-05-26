628b2b693bdeccc442e795e4931ea3a026d9ddc1
张敏 <zhangmin9x9@163.com>
2020-05-26 17:08:21 +0800

    配置多数据库来源

--- src/main/java/com/zmin/helloword/demo/service/ArticleRestJDBCServiceImpl.java
+++ src/main/java/com/zmin/helloword/demo/service/ArticleRestJDBCServiceImpl.java
@@ -3,6 +3,7 @@ package com.zmin.helloword.demo.service;
 import com.zmin.helloword.demo.dao.ArticleJDBCDAO;
 import com.zmin.helloword.demo.model.Article;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,31 +17,37 @@ public class ArticleRestJDBCServiceImpl implements ArticleRestService {
     @Resource
     ArticleJDBCDAO articleJDBCDAO;
 
+    @Resource
+    JdbcTemplate primaryJdbcTemplate;
+    @Resource
+    JdbcTemplate secondaryJdbcTemplate;
+
     @Transactional
     @Override
     public Article saveArticle(Article article) {
-        articleJDBCDAO.save(article);
+        articleJDBCDAO.save(article,primaryJdbcTemplate);
+        articleJDBCDAO.save(article,secondaryJdbcTemplate);
         log.info("saveArticle：{}", article);
         return article;
     }
 
     @Override
     public void deleteArticle(Long id) {
-        articleJDBCDAO.deleteById(id);
+        articleJDBCDAO.deleteById(id,primaryJdbcTemplate);
     }
 
     @Override
     public void updateArticle(Article article) {
-        articleJDBCDAO.updateById(article);
+        articleJDBCDAO.updateById(article,primaryJdbcTemplate);
     }
 
     @Override
     public Article getArticle(Long id) {
-        return articleJDBCDAO.findById(id);
+        return articleJDBCDAO.findById(id,primaryJdbcTemplate);
     }
 
     @Override
     public List<Article> getAll() {
-        return articleJDBCDAO.findAll();
+        return articleJDBCDAO.findAll(primaryJdbcTemplate);
     }
 }
