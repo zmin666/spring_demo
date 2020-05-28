6bb665bc25b30306ad056ec3137d7287838b38f7
张敏 <zhangmin9x9@163.com>
2020-05-28 14:24:31 +0800

    重回主分支

---
+++ src/main/java/com/zmin/helloword/demo/utils/DozerUtils.java
@@ -0,0 +1,24 @@
+package com.zmin.helloword.demo.utils;
+
+import com.google.common.collect.Lists;
+import org.dozer.DozerBeanMapperBuilder;
+import org.dozer.Mapper;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+public class DozerUtils {
+
+    static Mapper mapper = DozerBeanMapperBuilder.buildDefault();
+
+    public static <T> List<T> mapList(Collection sourceList, Class<T> destinationClass){
+        List destinationList = Lists.newArrayList();
+        for (Iterator i$ = sourceList.iterator(); i$.hasNext();){
+            Object sourceObject = i$.next();
+            Object destinationObject = mapper.map(sourceObject, destinationClass);
+            destinationList.add(destinationObject);
+        }
+        return destinationList;
+    }
+}
